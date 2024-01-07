8fa4f1e93e277e23cee2b92257d64fdd2141ef60
devilboi95 <turnickii.n@gmail>
2024-01-07 13:32:34 +0500

    Refactoring

--- OAuthConstructor/OAuthData.cs
+++ OAuthConstructor/OAuthData.cs
@@ -2,14 +2,14 @@ namespace UlearnTodoTimer.Infrasturcture.Services.AppAuth;
 
 public class OAuthData
 {
-    public string RedirectUrl = "http://localhost:5128/OAuth/Bot";
-    public string ResponseType = "code";
-    public string? Version = "5.131";
-    public string Scope = "friends";
+    public string RedirectUrl;
+    public string ResponseType;
+    public string? Version;
+    public string Scope;
     public string ClientSecret;
     public string ClientId;
-    public string ServiceOAuth = "https://oauth.vk.com";
-    public string UriAuthorization = "authorize";
-    public string UriGetAccessToken = "access_token";
-    public string Display = "page";
+    public string ServiceOAuth;
+    public string UriAuthorization;
+    public string UriGetAccessToken ;
+    public string Display;
 }
